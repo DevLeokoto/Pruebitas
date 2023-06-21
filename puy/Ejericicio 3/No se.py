5c02876a436802082bdae166ab980c252bffc153
DevLeokoto <120410978+DevLeokoto@users.noreply.github.com>
2023-06-21 16:39:09 -0400

    f

---
+++ puy/Ejericicio 3/No se.py
@@ -0,0 +1,6 @@
+a= input("Marico dime un número: ")
+b= input("Marico dime otro número y te dejo de joder: ")
+c= int(a)+int(b)
+print("Que numeros tan homosexuales. Na mentira\nLa suma de tus numeros es",c )
+
+
